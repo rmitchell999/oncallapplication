8837859aa2ec61e230921515bf401fc5250bf31c
rmitchell999 <rmitchell0402@gmail.com>
2025-01-20 09:55:51 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,7 @@ import "./assets/main.css";
 import { createApp } from 'vue';
 import App from './App.vue';
 import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
 import router from './router';
-
-// Configure Amplify with the correct exports
-Amplify.configure(aws_exports);
 
 // Create the Vue app and use the router
 createApp(App).use(router).mount("#app");
